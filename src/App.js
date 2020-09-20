12282d2b76d6e732de822f3884463e1bfa38a0ab
MDowling22 <65832586+MDowling22@users.noreply.github.com>
2020-09-19 20:57:28 -0700

    Updated to make weather app responsive.

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a href="www.linkedin.com/in/michelle-dowling-801499a7" target="_blank">
+            Michelle Dowling
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MDowling22/weather-react "
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://sleepy-borg-64e92f.netlify.app "
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
